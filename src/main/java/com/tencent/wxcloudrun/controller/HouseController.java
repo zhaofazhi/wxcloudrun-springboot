546d96846ee2beae3d2466dedc921f618b6e341e
zhaofazhi <897075002@qq.com>
2024-09-29 12:15:04 +0800

    第一次提交

--- src/main/java/com/tencent/wxcloudrun/controller/HouseController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/HouseController.java
@@ -17,10 +17,10 @@ public class HouseController {
         // 每页显示 6 个房源
         int pageSize = 6;
         String[] images = {
-                "https://ninja-1258454995.cos.ap-beijing.myqcloud.com/image.webp",
-                "https://ninja-1258454995.cos.ap-beijing.myqcloud.com/image2.webp",
-                "https://ninja-1258454995.cos.ap-beijing.myqcloud.com/image3.webp",
-                "https://ninja-1258454995.cos.ap-beijing.myqcloud.com/image4.webp"
+                "https://7072-prod-9gplmuz278725aad-1305749485.tcb.qcloud.la/image.webp?sign=08890e392ebe2420c68092f62bb7afc9&t=1727583248",
+                "https://7072-prod-9gplmuz278725aad-1305749485.tcb.qcloud.la/image2.webp?sign=55accd9961e43754947512c899d0b510&t=1727583228",
+                "https://7072-prod-9gplmuz278725aad-1305749485.tcb.qcloud.la/image3.webp?sign=129d253963b33bd464f9df4bed568674&t=1727583201",
+                "https://7072-prod-9gplmuz278725aad-1305749485.tcb.qcloud.la/image4.webp?sign=0d8d49440c4db21b639f349418907b79&t=1727583080"
         };
 
         List<House> houses = new ArrayList<>();
